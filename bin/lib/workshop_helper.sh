257233b534b08c2ea1d6aa949e0a1f86a3753705
Ken Alex Fassone <nexusventuri@gmail.com>
2020-12-27 12:15:30 +0000

    Extract workshop_helper
    Use same functions for run and run-all

---
+++ bin/lib/workshop_helper.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function show_explanation {
+  local file=$1
+  sed '/^-}/q' "$file" | sed  '2d;$d'
+  echo "-----------------------"
+}
+
+function wait_to_start {
+  echo ""
+  echo "Press enter to start"
+  read 
+}
+
+function execute_test {
+  local file=$1
+  local run_once=${2-'false'}
+  local cmd="elm-test $file"
+  local output=$($cmd 2>&1)
+
+  while grep -qie 'TEST RUN FAILED\|Compilation failed' <<< "$output";
+  do
+    echo -E "$output"
+
+    if [ "$run_once" == "false" ]
+    then
+      fswatch -1 -0 'src' #| xargs -0 -n 1 -I {} "pwd"
+      output=$($cmd 2>&1)
+    else
+      output=""
+    fi
+  done
+}
