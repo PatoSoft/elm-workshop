59c75df8bb06f55e6f47a4bee606b9242ddd20eb
Ken Alex Fassone <nexusventuri@gmail.com>
2020-12-29 10:24:55 +0000

    Fix colouring for elm compilation errors
    Instead of passing --color to elm-test I need to fake a tty
    because elm-test runs the compiler from here:
    
    https://github.com/rtfeldman/node-test-runner/blob/master/lib/Compile.js#L41
    
    I believe the elm binary does not accept the color and due to that
    colouring won't work on compilation errors.
    
    By 'faking' a tty I can force the elm compiler and elm-test to
    use colours as expected.

--- bin/lib/workshop_helper.sh
+++ bin/lib/workshop_helper.sh
@@ -12,10 +12,43 @@ function wait_to_start {
   read 
 }
 
+faketty () {
+  # copied from https://stackoverflow.com/a/57381061
+  tmp=$(mktemp)
+
+  # Ensure it worked or fail with status 99
+  [ "$tmp" ] || return 99
+
+  # Produce a script that runs the command provided to faketty as
+  # arguments and stores the status code in the temporary file
+  cmd="$(printf '%q ' "$@")"'; echo $? > '$tmp
+
+  # Run the script through /bin/sh with fake tty
+  if [ "$(uname)" = "Darwin" ]; then
+    # MacOS
+    script -Fq /dev/null /bin/sh -c "$cmd"
+  else
+    script -qfc "/bin/sh -c $(printf "%q " "$cmd")" /dev/null
+  fi
+
+  # Ensure that the status code was written to the temporary file or
+  # fail with status 99
+  [ -s $tmp ] || return 99
+
+  # Collect the status code from the temporary file
+  err=$(cat $tmp)
+
+  # Remove the temporary file
+  rm -f $tmp
+
+  # Return the status code
+  return $err
+}
+
 function execute_test {
   local file=$1
   local run_once=${2-'false'}
-  local cmd="elm-test $file --color"
+  local cmd="faketty elm-test $file"
   local output=$($cmd 2>&1)
 
   while grep -qie 'TEST RUN FAILED\|Compilation failed' <<< "$output";
