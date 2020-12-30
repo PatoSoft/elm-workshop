ee839f6932dfdc466cf54599556ffc80b14843bd
Ken Alex Fassone <nexusventuri@gmail.com>
2020-12-30 18:23:01 +0000

    Ensure elm-test is installed
    elm test is a strict dependency, when it is not available the error
    message does not appear and the whole workshop would look janky

--- bin/lib/dependency_helper.sh
+++ bin/lib/dependency_helper.sh
@@ -2,13 +2,14 @@
 
 function require_cmd() {
   local cmd=$1
-  local url=$2
+  local title=$2
+  local copy=$3
 
   if ! command -v $cmd &> /dev/null
   then
     echo "$cmd not found"
-    echo "please install it from here: "
-    echo "$url"
+    echo "$title"
+    echo "$copy"
     exit 0
   fi
 }
