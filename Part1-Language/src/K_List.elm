94d873ad72d8764f3c7c7f67461686a48cac3222
Ken Alex Fassone <nexusventuri@gmail.com>
2021-01-06 17:23:41 +0000

    Add information about myMap exercise

--- Part1-Language/src/K_List.elm
+++ Part1-Language/src/K_List.elm
@@ -36,3 +36,31 @@ tailTip items =
 wordsAndLengths : String -> List ( String, Int )
 wordsAndLengths sentence =
     []
+
+
+
+{-
+   TODO: Exercise 4
+   Implement myMap function by using pattern matching against a list and re-implementing map
+
+   Pattern matching on list can be used like this:
+
+     tailTip2 : List item -> Maybe item
+     tailTip2 items =
+         case items of
+             head :: [] ->
+                 Just head
+
+             head :: rest ->
+                 tailTip2 rest
+
+             _ ->
+                 Nothing
+
+   https://package.elm-lang.org/packages/elm/core/latest/List
+-}
+
+
+myMap : (item -> val) -> List item -> List val
+myMap fun items =
+    []
